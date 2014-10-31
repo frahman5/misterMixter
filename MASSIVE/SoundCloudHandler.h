91f1ab26812a59544850f0aca9334e65967963be
Faiyam Rahman <frahman305@gmail.com>
2014-10-30 22:47:51 -0400

    App is now compiling with soundcloud api included :-8. For future: you have to import <UIKit/UIKit.h> for it to work

---
+++ MASSIVE/SoundCloudHandler.h
@@ -0,0 +1,14 @@
+//
+//  SoundCloudHandler.h
+//  MASSIVE
+//
+//  Created by Faiyam Rahman on 10/30/14.
+//  Copyright (c) 2014 CornellTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface SoundCloudHandler : NSObject
+
+@end
---
+++ MASSIVE/SoundCloudHandler.m
@@ -0,0 +1,26 @@
+//
+//  SoundCloudHandler.m
+//  MASSIVE
+//
+//  Created by Faiyam Rahman on 10/30/14.
+//  Copyright (c) 2014 CornellTech. All rights reserved.
+//
+
+#import "SoundCloudHandler.h"
+#import "SCUI.h"
+
+@interface SoundCloudHandler ()
+
+@end
+
+@implementation SoundCloudHandler
+
+- (void) initializeSoundCloud {
+    [SCSoundCloud setClientID:@"7e4a3481d659fbcd9667741811dfa4ee"
+                       secret:@"ad15980c012fd968f0e33784e25b4551"
+                  redirectURL:[NSURL URLWithString:@"IMAXMassive://oauth"]];
+    
+    NSLog(@"soundcloud initalized");
+}
+
+@end
